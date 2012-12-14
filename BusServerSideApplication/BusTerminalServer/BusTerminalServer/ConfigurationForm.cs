bbb220d96c8b1e49447749e9a2496d9584436c29
ron123ald <ron123ald@gmail.com>
2012-12-14 19:40:44 +0800

    I just added the system design

---
+++ BusServerSideApplication/BusTerminalServer/BusTerminalServer/ConfigurationForm.cs
@@ -0,0 +1,106 @@
+﻿using System;
+namespace BusTerminalServer
+{
+    using System.Windows.Forms;
+
+    public partial class ConfigurationForm : Form
+    {
+        #region Serial Property
+        /// <summary>
+        /// </summary>
+        public string PortName
+        {
+            get { return this.PortNameBox.Text; }
+        }
+        /// <summary>
+        /// </summary>
+        public int BaudRate
+        {
+            get { return int.Parse(this.BaudRateBox.Text); }
+        }
+        /// <summary>
+        /// </summary>
+        public System.IO.Ports.Parity Parity
+        {
+            get { return (System.IO.Ports.Parity)this.ParityBox.SelectedIndex; }
+        }
+        /// <summary>
+        /// </summary>
+        public System.IO.Ports.StopBits StopBits
+        {
+            get { return (System.IO.Ports.StopBits)this.StopBitsBox.SelectedIndex; }
+        }
+
+        #endregion
+        /// <summary>
+        /// Constructor for ConfigurationForm
+        /// </summary>
+        public ConfigurationForm()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// Triggers whenever the user clicks on submit button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SubmitButton_Click(object sender, EventArgs e)
+        {
+            /// Message Box dialog result
+            DialogResult result = default(DialogResult);
+            try
+            {
+                /// Validate All Fields
+                /// Check if PortNameBox's value is Null or Empty
+                if (!string.IsNullOrEmpty(this.PortNameBox.Text))
+                {
+                    /// Check if BaudRateBox's value is Null or Empty
+                    if (!string.IsNullOrEmpty(this.BaudRateBox.Text))
+                    {
+                        /// Set Dialog result to Ok
+                        /// when dialoag result is Ok means Serial can now be Initialize.
+                        this.DialogResult = DialogResult.OK;
+                    }
+                    else
+                        /// Throw an Argument Null Exception for User to see that the the PortNameBox is NULL or Empty
+                        throw new ArgumentNullException("Baud Rate", "Baud Rate cannot be NULL or Empty");
+                }
+                else
+                    /// Throw an Argument Null Exception for User to see that the the PortNameBox is NULL or Empty
+                    throw new ArgumentNullException("Port Name", "Port name cannot be NULL or Empty");
+            }
+            catch (Exception ex)
+            {
+                /// Show Message Box with exception message details.
+                result = MessageBox.Show(this, ex.Message, "Exception Occured", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                /// Check the result of MessageBox
+                if (result == DialogResult.Retry)
+                {
+                    ClearComponent();
+                    /// Focus in the Port Name Box
+                    this.PortNameBox.Focus();
+                }
+                else if (result == DialogResult.Cancel)
+                {
+                    /// Set Dialog result to Ok
+                    /// when dialoag result is Ok means Serial can now be Initialize.
+                    this.DialogResult = result;
+                    this.Close();
+                }
+            }
+        }
+        /// <summary>
+        /// Clear Component 
+        ///   - Port Name Box
+        ///   - Baud Rate Box
+        /// </summary>
+        private void ClearComponent()
+        {
+            this.PortNameBox.Clear();
+            this.BaudRateBox.Clear();
+        }
+    }
+}
---
+++ BusServerSideApplication/BusTerminalServer/BusTerminalServer/MainForm.cs
@@ -0,0 +1,135 @@
+﻿namespace BusTerminalServer
+{
+    using System;
+    using System.Windows.Forms;
+    using System.IO.Ports;
+
+    public partial class MainForm : Form
+    {
+        /// <summary>
+        /// SerialPort Global declaration
+        /// </summary>
+        private SerialPort serial = default(SerialPort);
+        /// <summary>
+        /// Constructor of this Main Form
+        /// </summary>
+        public MainForm()
+        {
+            /// Internal Method ( auto generated );
+            InitializeComponent();
+        }
+        /// <summary>
+        /// this is an EventHandler method.
+        /// that triggers whenever the Main Form is Running
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Main_Load(object sender, EventArgs e)
+        {
+            /// Set DateTimer's Interval property to 100 Milliseconds
+            this.DateTimer.Interval = 100;
+            /// Starts the DateTimer ( System.Windows.Forms.Timer )
+            /// to Start Setting the current date of the program
+            this.DateTimer.Start();
+        }
+        /// <summary>
+        /// this is an EventHandler method
+        /// this will trigger whenever you click on Menu File->Configuration Option
+        /// A new Form will pop-up. your serial configuration will be base from the inputs 
+        /// on the new form.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void configurationToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            /// this will run through, once the serial instance is Null
+            if (this.serial == default(SerialPort))
+            {
+                ConfigurationForm config = new ConfigurationForm();
+                DialogResult result = config.ShowDialog();
+                /// If Dialog Result is Ok
+                /// Initialize Serial Port base from fields of ConfigurationForm
+                if (result == DialogResult.OK)
+                {
+                    /// Call InitializeSerialPort through Action
+                    /// this is an Asynchronous call of a method.
+                    /// means, this will never run in Main thread 
+                    /// instead this will run in a separate thread
+                    System.Action action = (() => InitializeSerialPort(config.PortName, config.BaudRate, config.Parity, config.StopBits));
+                    /// invoke action
+                    action.Invoke();
+                    /// Close the ConfigurationForm Instance
+                    config.Close(); 
+                }
+                /// Show Warning message
+                /// Close the Program
+                else
+                {
+
+                }
+            }
+        }
+        /// <summary>
+        /// this is an EventHandler method
+        /// this will trigger whenever you click on Menu File->Exit Option
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            /// Close the Application
+            this.Close();
+        }
+        /// <summary>
+        /// this is an EventHandler Method.
+        /// this will trigger every 100 milliseconds and 
+        /// set the DateTimeLabel's value text to the 
+        /// current time base on your System unit's date and time
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void DateTimer_Tick(object sender, EventArgs e)
+        {
+            /// Set the DateTimeLabel Component to the current Date and Time value
+            this.DateTimeLabel.Text = DateTime.Now.ToString();
+
+            if (this.serial != null && this.serial.IsOpen)
+            {
+                string data = this.serial.ReadLine();
+            }
+        }
+        /// <summary>
+        /// Initialize SerialPort
+        /// </summary>
+        /// <param name="portname">Name of the Port</param>
+        /// <param name="baudrate"></param>
+        /// <param name="parity"></param>
+        /// <param name="stopbits"></param>
+        private void InitializeSerialPort(string portname, int baudrate, Parity parity, StopBits stopbits)
+        {
+            /// Initialize Serial by Params
+            this.serial = new SerialPort();
+            /// set portname property
+            this.serial.PortName = portname;
+            /// set baudrate property
+            this.serial.BaudRate = baudrate;
+            /// set parity property
+            this.serial.Parity = parity;
+            /// set stopbits property
+            this.serial.StopBits = stopbits;
+            /// Open SerialPort Instance
+            this.serial.Open();
+            /// Initialize SerialPort EventHandlers
+            /// serial DataReceived
+            this.serial.DataReceived += new SerialDataReceivedEventHandler(serial_DataReceived);
+        }
+        /// <summary>
+        /// Data Received from Serial Communication Port
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void serial_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+        }
+    }
+}
