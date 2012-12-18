f7021b6b1e5a95ca79fff03b4dcab00c554f82c9
ronald curitao <dna.ronron@gmail.com>
2012-12-17 21:16:50 -0800

    Deleted some unsued file

--- Library/BusTerminalLibrary/BusTerminalLibrary/BusTerminal.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BusTerminalLibrary
-{
-    using System;
-
-    public delegate void Initializing(object sender, EventArgs e);
-    public delegate void UpdateLocation(object sender, EventArgs e);
-    public delegate void ServerStartEventHandler(object sender, EventArgs e);
-
-}
